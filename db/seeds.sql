7cfb2db6097bde49c82f36acdc771b54332522e8
Mike Z <mzapata2991@gmail.com>
2019-06-16 20:36:39 -0500

    db created

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Cheeseburger', false);
