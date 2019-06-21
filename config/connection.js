aeae20e09e4ca49b9e2a20905fa52572a8a8ac33
Mike Z <mzapata2991@gmail.com>
2019-06-21 16:04:45 -0500

    some styling done

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    datbase: 'burgers_db'
+  });
+};
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
@@ -10,7 +22,7 @@ var connection = mysql.createConnection({
 });
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
