8dab933ee350719c5e64fdeb73f20828a8f9c289
Mike Z <mzapata2991@gmail.com>
2019-06-22 11:40:39 -0500

    removed duplicate connection

--- config/connection.js
+++ config/connection.js
@@ -13,13 +13,13 @@ if (process.env.JAWSDB_URL) {
    });
  };
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 8889,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function (err) {
