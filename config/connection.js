a1f0180245c9abf0bb89616043e60a640fbe0060
Mike Z <mzapata2991@gmail.com>
2020-04-09 10:51:44 -0500

    updated pkgs

--- config/connection.js
+++ config/connection.js
@@ -13,13 +13,6 @@ if (process.env.JAWSDB_URL) {
    });
  };
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 8889,
-//   user: "root",
-//   password: "root",
-//   database: "burgers_db"
-// });
 
 // Make connection.
 connection.connect(function (err) {
--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,4 @@
-// Import the ORM to create functions that will interact with the database.
+`// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 var burger = {
@@ -13,13 +13,7 @@ var burger = {
       cb(res);
     });
   },
-  // insertOne: function(name, cb) {
-  //   orm.create("burgers", [
-  //     "burger_name", "devoured"
-  //   ], [
-  //     name, false
-  //   ], cb);
-  // },
+ 
   updateOne: function (objColVals, condition, cb) {
     orm.updateOne("burgers", objColVals, condition, function (res) {
       cb(res);
